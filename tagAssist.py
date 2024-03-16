021dc1127264264fb5c2ba45721918b88dddd2d3
Pip <leos32111@gmail.com>
2024-03-16 17:44:13 -0400

    Implemented tag-request to tag-output

--- tagAssist.py
+++ tagAssist.py
@@ -6,13 +6,18 @@ import logging.handlers
 import discord
 from dotenv import load_dotenv
 
+from steam import Steam
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+STEAM_TOKEN = os.getenv('STEAM_API_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents = intents)
+
+steam = Steam(STEAM_TOKEN)
 
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
@@ -20,9 +25,26 @@ logger.setLevel(logging.DEBUG)
 handler = logging.handlers.RotatingFileHandler("log/errors.log", maxBytes=1024, backupCount=2)
 logger.addHandler(handler)
 
-async def send_message(original_message, fixed_message):
-    await original_message.channel.send(fixed_message)
-    await original_message.delete()
+#From: https://gist.github.com/bcahue/4eae86ae1d10364bb66d
+steamid64ident = 76561197960265728
+def commid_to_steamid(commid):
+  steamid = []
+  steamid.append('STEAM_0:')
+  steamidacct = int(commid) - steamid64ident
+  
+  if steamidacct % 2 == 0:
+      steamid.append('0:')
+  else:
+      steamid.append('1:')
+  
+  steamid.append(str(steamidacct // 2))
+  
+  return ''.join(steamid)
+
+async def send_message(msg, original_msg):
+    channel = discord.utils.get(client.get_all_channels(), name = 'tag-output')
+    await channel.send(msg)
+    await original_msg.add_reaction('\N{THUMBS UP SIGN}')
 
 @client.event
 async def on_message(message):
@@ -32,23 +54,42 @@ async def on_message(message):
         
         new_message = None
 
-        if("https://twitter.com/" in message.content and "/status/" in message.content):
-            #To maintain original message, find twitter link index
-            twit_idx = message.content.index("https://twitter") + 8
-            new_message = f"From {message.author.mention}: {message.content[0:twit_idx]}vx{message.content[twit_idx:]}"
-        elif("https://x.com" in message.content and "/status/" in message.content):
-            #To maintain original message, find x link index
-            x_idx = message.content.index("https://x") + 8
-            new_message = f"From {message.author.mention}: {message.content[0:x_idx]}vxtwitter{message.content[x_idx + 1:]}"
-        elif("https://www.tiktok.com/" in message.content):
-            #To maintain original message, find o in tiktok index
-            tik_idx = message.content.index("https://www.tiktok") + 16
-            new_message = f"From {message.author.mention}: {message.content[0:tik_idx]}x{message.content[tik_idx + 1:]}"
+        if("https://steamcommunity.com/" in message.content and message.channel.name == 'tag-request'):
+            lines = str(message.content).splitlines()
+            tag = color = steam_url = None
+
+            #Parse message
+            for line in lines:
+                lower_line = line.lower()
+                if('tag' in lower_line):
+                    tag = line.split(':')[-1].lstrip(' ')
+                elif('color' in lower_line):
+                    color = line.split(':')[-1].replace('#', '').replace(' ','')
+                elif('steam' in lower_line):
+                    steam_url = line.split(':')[-1].replace(' ','')
+                
+            #ERROR CHECK
+            assert tag != None
+            assert color != None
+            assert steam_url != None
+
+            #Get steam id info
+            steam_url_id = steam_url.split('/')[-2]
+            steam_id_64 = steam_url_id if str(steam_url_id).isnumeric() else steam.users.get_steamid(steam_url_id)['steamid']
+            steam_id = commid_to_steamid(steam_id_64)
+
+            #Construct config entry
+            first_line = '\"' + steam_id + '\"\t// ' + str(message.author) +'\n{\n'
+            tag_line = '\t\"tag\"\t\t\"[' + tag + '] \"\n'
+            namecolor_line = '\t\"namecolor\"\t\t\"\"\n'
+            tagcolor_line = '\t\"tagcolor\"\t\t\"' + color + '\"\n}'
+            
+            new_message = first_line + tag_line + namecolor_line + tagcolor_line
         
         if new_message:
-            await send_message(message, new_message)
-            
+            await send_message(new_message, message)
+                
     except Exception as e:
         logger.error(f"{datetime.now()}: {e}\t{message}")
         
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
